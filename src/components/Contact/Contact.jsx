6620191fee8338a3e86e8237638df2c7d91969a9
Edward Guo <edward.h.guo@gmail.com>
2017-02-14 14:44:52 -0800

    contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import './_Contact.scss';
 
-export default class Contact extends Component {
-  render() {
-    return (
-      <div>Contact</div>
-    );
-  }
+export default function Contact() {
+  return (
+    <div className="contact">
+      <div>
+        <h1>Contact Information:</h1>
+        <p>Email: info@neat-consulting.com</p>
+        <p>Phone: 562-338-0706</p>
+      </div>
+    </div>
+  );
 }
